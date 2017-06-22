d81331c1ad82f3328ed6bc4b0d817b98aa7fabea
yangxutao <yangxutao361@163.com>
2017-06-22 17:23:56 +0800

    c

---
+++ coding_test/src/rootVC/YTMessageViewController.h
@@ -0,0 +1,13 @@
+//
+//  YTMessageViewController.h
+//  coding_test
+//
+//  Created by yangxutao on 2017/6/22.
+//  Copyright © 2017年 yangxutao. All rights reserved.
+//
+
+#import "YTBaseViewController.h"
+
+@interface YTMessageViewController : YTBaseViewController
+
+@end
---
+++ coding_test/src/rootVC/YTTastViewController.h
@@ -0,0 +1,13 @@
+//
+//  YTTastViewController.h
+//  coding_test
+//
+//  Created by yangxutao on 2017/6/22.
+//  Copyright © 2017年 yangxutao. All rights reserved.
+//
+
+#import "YTBaseViewController.h"
+
+@interface YTTastViewController : YTBaseViewController
+
+@end

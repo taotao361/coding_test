a41f94cb5dcd6495324712c6ba1e17a1df88b525
yangxutao <yangxutao361@163.com>
2017-06-22 15:22:49 +0800

    add

---
+++ coding_test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  coding_test
+//
+//  Created by yangxutao on 2017/6/22.
+//  Copyright © 2017年 yangxutao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
